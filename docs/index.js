1092a6a22e99c1d567f8eda17af4fe702f712bb5
ErrorFlynn <ErrorFlynn@users.noreply.github.com>
2019-08-29 16:36:35 -0400

    Add files via upload

--- docs/index.js
+++ docs/index.js
@@ -1,61 +1,162 @@
-window.onload = function()
+onload = function()
 {
-	var toggler = document.getElementsByClassName("caret");
+	var toggler = document.getElementsByClassName('caret');
 	var i;
 
 	for (i = 0; i < toggler.length; i++)
 	{
-		toggler[i].addEventListener("click", function()
+		toggler[i].addEventListener('click', function()
 		{
-			this.parentElement.querySelector(".nested").classList.toggle("active");
-			this.classList.toggle("caret-down");
+			this.parentElement.querySelector(".nested").classList.toggle('active');
+			this.classList.toggle('caret-down');
 		});
 	}
 
+	onpopstate = onPopState;
+	onPopState.skip = false;
+
+	var TOCLinks = document.querySelectorAll('[data-href]');
+	for(let link of TOCLinks)
+	{
+		link.onclick = function()
+		{
+			if(!link.classList.contains('selected'))
+			{
+				onPopState.skip = true;
+				let href = this.getAttribute('data-href');
+				frames[0].location.replace(href);
+			}
+		};
+	}
+
 	var startDoc = 'widget_listbox.html';
-	var loc = window.location.href;
+	fnTarget = new String();
+	var loc = location.href;
 	var pos = loc.indexOf('#');
 	if(pos != -1)
 	{
-		startDoc = loc.substr(pos+1);
+		let pos2 = loc.indexOf('::');
+		if(pos2 != -1)
+		{
+			startDoc = loc.substring(pos+1, pos2);
+			fnTarget = loc.substr(pos2+2);
+		}
+		else startDoc = loc.substring(pos+1);
+
 		if(startDoc.toLowerCase().indexOf('.html') == -1)
 			startDoc += '.html';
 	}
 
 	var iframe = document.getElementsByTagName('iframe')[0];
-	iframe.onload = makeSideBar;
+	iframe.onload = iframeOnLoad;
 	iframe.setAttribute('src', startDoc);
 	
-	window.onresize = function()
-	{
-		var fdoc = window.frames[0].window.document;
-		var main = fdoc.getElementsByTagName('main')[0];
-		var nav = fdoc.getElementsByTagName('nav')[0];
-
-		if(typeof this.navHidden == 'undefined')
-			this.navHidden = false;
-		if(document.getElementById('frame').clientWidth < 1325)
-		{
-			if(!this.navHidden)
-			{
-				this.navHidden = true;
-				nav.style.display = 'none';
-				main.style.paddingRight = '0';
-			}
-		}
-		else if(this.navHidden)
-		{
-			this.navHidden = false;
-			nav.style.display = 'table';
-			main.style.paddingRight = '300px';
-		}
-	};
-	window.onresize();
+	onresize = onResize;
+	onResize();
 };
+
+function onResize()
+{
+	var fdoc = frames[0].document;
+	var main = fdoc.getElementsByTagName('main')[0];
+	var nav = fdoc.getElementsByTagName('nav')[0];
+
+	if(typeof onResize.navHidden == 'undefined')
+		onResize.navHidden = false;
+	if(document.getElementById('frame').clientWidth < 1325)
+	{
+		if(!onResize.navHidden)
+		{
+			onResize.navHidden = true;
+			nav.style.display = 'none';
+			main.style.paddingRight = '0';
+		}
+	}
+	else if(onResize.navHidden)
+	{
+		onResize.navHidden = false;
+		nav.style.display = 'table';
+		main.style.paddingRight = '300px';
+	}
+}
+
+function onPopState()
+{
+	if(onPopState.skip)
+	{
+		onPopState.skip = false;
+		return;
+	}
+	let frameDoc = frames[0].location.href;
+	frameDoc = frameDoc.substring(frameDoc.lastIndexOf('/') + 1);
+	let curDoc = location.hash.substring(1);
+	let pos = curDoc.indexOf('::');
+	if(pos != -1)
+		curDoc = curDoc.substring(0, pos);
+	if(frameDoc != curDoc) location.reload();
+}
+
+function iframeOnLoad()
+{
+	document.title = frames[0].document.title;
+	setTOCCursor();
+	makeSideBar();
+
+	if(fnTarget !== 'undefined')
+		scrollToFunction(fnTarget);
+
+	if(typeof this.first === 'undefined')
+	{
+		this.first = false;
+		return;
+	}
+
+	let href = frames[0].location.href;
+	href = href.substring(href.lastIndexOf('/')+1);
+	let loc = location.href;
+	let pos = loc.indexOf('#');
+	if(pos != -1)
+		loc = loc.substring(0, pos);
+	loc += '#' + href.substring(0, href.lastIndexOf('.html'));
+	location.assign(loc);
+}
+
+function setTOCCursor(docName)
+{
+	var selected = document.querySelector('.selected');
+	if(selected)
+		selected.classList.toggle('selected');
+
+	if(typeof docName === 'undefined')
+	{
+		var docName = frames[0].location.href;
+		docName = docName.substring(docName.lastIndexOf('/') + 1);
+	}
+
+	var target = document.querySelector('[data-href="' + docName + '"]');
+	if(target)
+	{
+		target.classList.toggle('selected');
+		let ul = target.parentElement.parentElement;
+		let span = ul.parentElement.firstElementChild;
+		if(!span.classList.contains('caret-down'))
+		{
+			ul.classList.toggle('active');
+			span.classList.toggle('caret-down');
+		}
+		ul = ul.parentElement.parentElement;
+		span = ul.parentElement.firstElementChild;
+		if(span.nodeName === 'SPAN' && !span.classList.contains('caret-down'))
+		{
+			ul.classList.toggle('active');
+			span.classList.toggle('caret-down');
+		}
+	}
+}
 
 function makeSideBar()
 {
-	var fdoc = window.frames[0].window.document;
+	var fdoc = frames[0].document;
 	var rows = fdoc.querySelectorAll('tr.clickable-row');
 	if(rows.length)
 	{
@@ -83,20 +184,7 @@ function makeSideBar()
 			li.addEventListener('click', function()
 			{
 				let section = fdoc.querySelector('section[expanded]');
-				if(section)
-				{
-					let el = section.querySelector('h2');				
-					if(el.textContent.charAt(1) == '+') // expanding
-					{
-						el.style['margin-bottom'] = '';
-						el.textContent = el.textContent.replace('+', '-');
-						let siblings = section.querySelectorAll('h2 ~ *');
-						siblings.forEach(sibling =>
-						{
-							sibling.removeAttribute('hidden');
-						});
-					}
-				}
+				if(section) expandSection(section);
 
 				let row = rows[items[this.textContent]];
 				for(let r of rows)
@@ -110,6 +198,41 @@ function makeSideBar()
 					hiddenRow.removeAttribute('hidden');
 				hiddenRow.scrollIntoView({/*behavior: 'smooth', */block: 'center'});
 			});
+		}
+	}
+}
+
+function expandSection(section)
+{
+	var el = section.querySelector('h2');
+	if(el.textContent.charAt(1) == '+') // expanding
+	{
+		el.style['margin-bottom'] = '';
+		el.textContent = el.textContent.replace('+', '-');
+		let siblings = section.querySelectorAll('h2 ~ *');
+		siblings.forEach(sibling =>
+		{
+			sibling.removeAttribute('hidden');
+		});
+	}
+}
+
+function scrollToFunction(name)
+{
+	var fdoc = frames[0].document;
+	var spans = fdoc.querySelectorAll('span.clickable-cell');
+	
+	for(let span of spans)
+	{
+		if(span.textContent === name)
+		{
+			let section = span.parentElement.parentElement.parentElement.parentElement.parentElement;
+			expandSection(section);
+			let hiddenRow = span.parentElement.parentElement.nextElementSibling;
+			if(hiddenRow.hasAttribute('hidden'))
+				hiddenRow.removeAttribute('hidden');
+			hiddenRow.scrollIntoView({block: 'center'});
+			return;
 		}
 	}
 }
